dfe2e680d76a8ec9b740c3090dab76b678e9e05f
SylvestreLucas <57163480+SylvestreLucas@users.noreply.github.com>
2021-04-18 19:56:01 -0300

    Create Bebidas.java

---
+++ Classes/Bebidas.java
@@ -0,0 +1,25 @@
+package Classes;
+
+public class Bebidas {
+	
+	public String marca;
+	public String tipo;
+	public int tamanho;
+	
+	
+	void Disponibilidade() {
+		
+	}
+	
+	void perco() {
+		
+	}
+	
+	void quantidade() {
+		
+	}
+	
+	void AdicionarSacola() {
+		
+	}
+}
