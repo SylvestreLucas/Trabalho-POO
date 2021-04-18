f581fcea3b689e4f94404674953a6efb26de3ede
SylvestreLucas <57163480+SylvestreLucas@users.noreply.github.com>
2021-04-18 19:56:53 -0300

    Create localizacao.java

---
+++ Classes/localizacao.java
@@ -0,0 +1,16 @@
+package Classes;
+
+public class Localizacao {
+	public String CEP;
+	public String Rua;
+	public String Numero;
+	public String Estado;
+	public String Pais;
+	public String BLap;
+	
+	
+	
+	void SetarLocalizacao() {
+
+}
+}
