695e3dfcdd6bae77289ff66a354842ddbe9c0405
SylvestreLucas <57163480+SylvestreLucas@users.noreply.github.com>
2021-04-18 20:22:18 -0300

    Create sacola.java

---
+++ Classes/sacola.java
@@ -0,0 +1,42 @@
+package Classes;
+
+public class Sacola{
+	public String usuario;
+	public String Localizacao;
+	public String Produtos;
+	public String EnderecodeEntrega;
+	public String carrinho;
+	public String frete;
+	public float precoTotal;
+	public String Pagamento;
+	
+	
+	
+	void frete() {
+		
+	}
+	
+	void precoTotal() {
+		
+	}
+	
+	void Pagamento() {
+		
+	}
+	
+	void InstrucaoEntregador() {
+		
+	}
+	
+	void NumeroPedido() {
+		
+	}
+	
+	void Status() {
+		
+	}
+		
+	
+	
+	}
+	
